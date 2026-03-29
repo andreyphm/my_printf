81b0cd037348965858223ade2d731c7808943b70
andreyphm <andrewkrokhin@gmail.com>
2026-03-30 00:36:34 +0300

    Add stdio.h printf for comparison in main.

--- main.c
+++ main.c
@@ -4,6 +4,7 @@ long my_printf(const char* format_string, ...);
 
 int main()
 {
-    my_printf("Misha %d %x %o %b %c %s %%\n", 666, 0x200, 100, 16, 'Q', "bad_pig");
+    my_printf("Misha %d %x %o %c %s %%\n", 666, 0x200, 16, 'Q', "bad_pig");
+    printf("Misha %d %x %o %c %s %%\n", 666, 0x200, 16, 'Q', "bad_pig");
     return 0;
 }
